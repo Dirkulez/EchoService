6a3c9221b8462d4f4b6920b4c359228f05a596fa
watteroth.dirk <watteroth.dirk@IBM305-13>
2018-10-05 11:39:00 +0200

    Nutzung EchoService implementiert

--- EchoService/Program.cs
+++ EchoService/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EchoService.ServiceReference1;
 
 namespace EchoService
 {
@@ -10,6 +11,11 @@ namespace EchoService
     {
         static void Main(string[] args)
         {
+            //EchoServiceClient ist in WSDL beschrieben 
+            EchoServiceClient client = new EchoServiceClient();
+            var result = client.Echo("Hello World");
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
